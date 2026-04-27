40dbb81665158dee0c532ffe12e8ecf969b1d62e
Santiago Romero <santiagoromero@sandiego.edu>
2026-04-26 18:14:38 -0700

    deleted loop

--- src/main/java/edu/sandiego/comp305/StringUtils.java
+++ src/main/java/edu/sandiego/comp305/StringUtils.java
@@ -2,6 +2,7 @@ package edu.sandiego.comp305;
 
 public final class StringUtils {
     public static String reverseString(final String input) {
+
         return new StringBuilder(input).reverse().toString();
     }
 }
