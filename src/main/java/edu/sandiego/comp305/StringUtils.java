7296fcbae477e6f0a700ef3875cca50c3592029f
Santiago Romero <santiagoromero@sandiego.edu>
2026-04-26 18:31:44 -0700

    fix: Added empty line to end of StringUtils and StringUtilsTest. Added javadoc for the code

--- src/main/java/edu/sandiego/comp305/StringUtils.java
+++ src/main/java/edu/sandiego/comp305/StringUtils.java
@@ -1,8 +1,23 @@
 package edu.sandiego.comp305;
 
+/**
+ * Utility class for String operations.
+ */
 public final class StringUtils {
-    public static String reverseString(final String input) {
 
+    /**
+     * Private constructor
+     */
+    private StringUtils() {
+    }
+
+    /**
+     * Reverses the given string.
+     *
+     * @param input string to reverse
+     * @return reversed string
+     */
+    public static String reverseString(final String input) {
         return new StringBuilder(input).reverse().toString();
     }
 }
